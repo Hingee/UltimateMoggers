f93149dd726f680b950c9ef3197b09f238bb421a
Nathan Hingee <nathan.hingee@students.mq.edu.au>
2024-04-25 12:36:42 +1000

    Updated names and Design Diagram

--- src/Role.java
+++ src/Role.java
@@ -2,17 +2,17 @@ package src;
 
 public enum Role {
     NA, //Error Margins
-    Villager, //No special actions
-    Witch, //Can heal or poison only 1 potion each per GAME
-    Seer, //Can see a players team
-    ApprenticeSeer, //picks 2 players and checks if they on the same team
-    Fool, //If the village votes against her, she turns over her card (doesn't die) but now can't vote
-    Hunter, //Can kill another player if killed
-    Thief, //At the beginning of the game, thief can choose from 2 cards placed in the center
-    Mason, // Can see the other mason
-    Elder,  //Must be killed 2x by wolves
-    Defender, //Can guard 1 person at night cannot guard themself 2 nights in a row
-    Werewolf, //Kills player during the night
-    AlphaWerewolf, //Only alpha vote counts, creates new wolf instead of killing the person
-    BigBadWolf, //Wolf vote counts as 2
+    NPC, //No special actions
+    Doctor, //Can heal or poison only 1 potion each per GAME
+    Detective, //Can see a players team
+    ApprenticeDetective, //picks 2 players and checks if they on the same team
+    Degenerate, //If the villages votes against them, they turns over their card (doesn't die) but now can't vote
+    Aggresor, //Can moggs another player if mogged
+    TwoFacer, //At the beginning of the game, TwoFacer can choose from 2 cards placed in the center
+    Twins, // Can see the other twin
+    Mewer,  //Must be mogged x2
+    Guarder, //Can guard 1 person at night cannot guard themself 2 nights in a row
+    Mogger, //Moggs player during the night
+    AlphaMogger, //Only alpha vote counts, convertes a new mogger instead of mogging the person
+    BullyMogger, //Vote counts as 2
 }
